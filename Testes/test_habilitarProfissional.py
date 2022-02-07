c789efa2496c9ff4aae9622e0c6e6220584e3f6b
cleocardos <cleotads21@gmail.com>
2022-02-07 09:58:20 -0300

    testes de habilitar profissional

--- Testes/test_habilitarProfissional.py
+++ Testes/test_habilitarProfissional.py
@@ -43,6 +43,35 @@ class TestHabilitarProfissional(unittest.TestCase):
         #assert "Perfil profissional adicionado com sucesso!" in result
         self.assertTrue("Perfil profissional adicionado com sucesso!" in result)
 
+    @allure.testcase("Testando o Campos habilitar profissional campos vazio")
+    def test_habilitar_profissional_campos_vazio(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000/submit_login/")
+
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys("teste")
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("123456")
+
+        driver.find_element_by_xpath('//*[@id="submit_login"]').click()
+        time.sleep(2)
+        driver.forward()
+
+        time.sleep(2)
+        driver.find_element_by_xpath('/html/body/aside/ul/li[2]/a/span').click()
+
+        driver.forward()
+
+        driver.find_element_by_xpath('//*[@id="id_profissao"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="id_categoria"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="id_imagem"]').send_keys("")
+        driver.find_element_by_xpath('//*[@id="id_descricao"]').send_keys("")
+
+        driver.find_element_by_xpath('//html/body/main/div[2]/div/div/div/div/form/div[5]/button').click()
+
+        result = driver.find_element_by_class_name('alert-danger').text
+
+
+        self.assertTrue("Preencha os campos!" in result)
+
     def tearDown(self):
         self.driver.close()
 
