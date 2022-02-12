09e99dd9529cb860d2f0d1075f34922b63e561c4
cleocardos <cleotads21@gmail.com>
2022-02-12 09:30:24 -0300

    inserindo testes de avaliar profissional

--- Testes/test_avaliarprofissional.py
+++ Testes/test_avaliarprofissional.py
@@ -41,8 +41,8 @@ class TestAvaliarProfissional(unittest.TestCase):
 
         self.assertTrue('Avaliação realizada com sucesso!')
 
-    @allure.testcase("Avaliar Profissional vazio")
-    def test_Avaliar_profissional_vazio(self):
+    @allure.testcase("Avaliar Profissional campo descrição vazio")
+    def test_Avaliar_profissional_descrição_vazio(self):
         driver = self.driver
         driver.get("http://127.0.0.1:8000/submit_login/")
 
@@ -93,7 +93,6 @@ class TestAvaliarProfissional(unittest.TestCase):
 
         self.assertTrue("Nenhuma Avaliação encontrada!" is not result)
 
-
     @allure.testcase("Testando visualizar avaliacoes profissional vazio")
     def test_visualizar_avaliacoes_profissional_vazio(self):
         driver = self.driver
@@ -115,6 +114,126 @@ class TestAvaliarProfissional(unittest.TestCase):
         #print("RESULT--->", result)
 
         self.assertTrue("Nenhuma Avaliação encontrada!" in result)
+
+    @allure.testcase("Avaliar Profissional nota minima")  # não foi possivel realizar
+    def test_Avaliar_profissional_nota_min(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000/submit_login/")
+
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys("teste")
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("123456")
+
+        driver.find_element_by_xpath('//*[@id="submit_login"]').click()
+        time.sleep(2)
+        driver.forward()
+
+        driver.find_element_by_name("termo").send_keys("manicure")
+        driver.find_element_by_name("termo").send_keys(Keys.RETURN)
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[1]/div/div/div/div/a/h5').click()
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[3]/div/div/div/div/div[2]/a[2]').click()
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[1]/div/label[1]/i').click()
+        driver.find_element_by_xpath('//*[@id="id_descricao"]').send_keys("Bom")
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[3]/button').click()
+
+        result = driver.find_element_by_xpath('')
+
+        self.assertTrue('Avaliação realizada com sucesso!')
+
+    @allure.testcase("Avaliar Profissional nota maxima")  # não foi possivel realizar
+    def test_Avaliar_profissional_nota_max(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000/submit_login/")
+
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys("teste")
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("123456")
+
+        driver.find_element_by_xpath('//*[@id="submit_login"]').click()
+        time.sleep(2)
+        driver.forward()
+
+        driver.find_element_by_name("termo").send_keys("manicure")
+        driver.find_element_by_name("termo").send_keys(Keys.RETURN)
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[1]/div/div/div/div/a/h5').click()
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[3]/div/div/div/div/div[2]/a[2]').click()
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[1]/div/label[5]').click()
+        driver.find_element_by_xpath('//*[@id="id_descricao"]').send_keys("Bom")
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[3]/button').click()
+
+        result = driver.find_element_by_xpath('')
+
+        self.assertTrue('Avaliação realizada com sucesso!')
+
+    @allure.testcase("Avaliar Profissional descricao minima")  # não foi possivel realizar
+    def test_Avaliar_profissional_descricao_min(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000/submit_login/")
+
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys("teste")
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("123456")
+
+        driver.find_element_by_xpath('//*[@id="submit_login"]').click()
+        time.sleep(2)
+        driver.forward()
+
+        driver.find_element_by_name("termo").send_keys("manicure")
+        driver.find_element_by_name("termo").send_keys(Keys.RETURN)
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[1]/div/div/div/div/a/h5').click()
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[3]/div/div/div/div/div[2]/a[2]').click()
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[1]/div/label[5]').click()
+        driver.find_element_by_xpath('//*[@id="id_descricao"]').send_keys("B")
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[3]/button').click()
+
+        result = driver.find_element_by_xpath('')
+
+        self.assertTrue('Avaliação realizada com sucesso!')
+
+    @allure.testcase("Avaliar Profissional descricao maxima")  # não foi possivel realizar
+    def test_Avaliar_profissional_descricao_max(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:8000/submit_login/")
+
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys("teste")
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys("123456")
+
+        driver.find_element_by_xpath('//*[@id="submit_login"]').click()
+        time.sleep(2)
+        driver.forward()
+
+        driver.find_element_by_name("termo").send_keys("manicure")
+        driver.find_element_by_name("termo").send_keys(Keys.RETURN)
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[1]/div/div/div/div/a/h5').click()
+
+        driver.forward()
+        driver.find_element_by_xpath('/html/body/main/div[3]/div/div/div/div/div[2]/a[2]').click()
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[1]/div/label[5]').click()
+        driver.find_element_by_xpath('//*[@id="id_descricao"]').send_keys("B" * 251)
+
+        driver.find_element_by_xpath('//*[@id="show_class"]/div[3]/button').click()
+
+        result = driver.find_element_by_xpath('')
+
+        self.assertTrue('Avaliação realizada com sucesso!')
 
     def tearDown(self):
         self.driver.close()
